9e3dc38c42956dd5d29e0d321ac568d803ebc0ad
MatlhogonoloBokaba <ST10510894@rcconnect.edu.za>
2026-04-13 09:54:06 +0200

    AccountLogin.java

--- AccountLogin.java
+++ AccountLogin.java
@@ -14,45 +14,54 @@ public class AccountLogin {
     
     public static void main(String[] args) {
          Scanner myInput = new Scanner(System.in);
-         
+
+        // This is where User inputs their name
          System.out.println("Enter First name: ");
          String firstName = myInput.nextLine();
          
-         System.out.println("Enter Last name: ");
+       // This is where User inputs their last name
+        System.out.println("Enter Last name: ");
          String lastName = myInput.nextLine();
          
-         System.out.println("Enter your username: ");
+        // This is where User creates a username
+        System.out.println("Enter your username: ");
          String username = myInput.nextLine();
          
-         if(username.contains("_") && username.length() <=5 ) {
+       // Username If statements
+        if(username.contains("_") && username.length() <=5 ) {
              System.out.println("Username successfully captured.");
          }else {
              System.out.println("Username is not correctly formatted;please ensure that your username contains an underscore and is no more than five characters in length.");
          }
          
-         System.out.println("Create a password: ");
+       // This is where the User creates a password
+        System.out.println("Create a password: ");
          String password = myInput.nextLine();
          
-         if(password != null && password.matches(".*[0-9].*") && password.length() >=8 && password.matches(".*[!@#$%].*") &&  password.matches(".*[A-Z].*")) {
+       // Password If statements
+        if(password != null && password.matches(".*[0-9].*") && password.length() >=8 && password.matches(".*[!@#$%].*") &&  password.matches(".*[A-Z].*")) {
              System.out.println("Password successfully captured.");
          }else {
              System.out.println("Password is not correctly formatted;please ensure that the password contains atleast eight characters,a capital letter,a number,,and a special character.");
          }
          
-         System.out.println("Enter your number: ");
+      // This is where the User inputs their cell phone number
+        System.out.println("Enter your number: ");
          String number = myInput.nextLine();
          
-         String regex = "^\\+\\d{1,3}\\d{1,10}";
+      // Setting the regex to check the cell phone number and check for patterns
+        String regex = "^\\+\\d{1,3}\\d{1,10}";
          
          Pattern pattern = Pattern.compile(regex);
          Matcher matcher = pattern.matcher(number);
          
-         if(number != null && matcher.matches() && number.length() == 12) {
+     // Number If statements
+        if(number != null && matcher.matches() && number.length() == 12) {
              System.out.println("Cell phone number successfully added.");
          }else {
              System.out.println("Cell phone number is incorrectly formatted or does not include an international code.");
          }
-                    
+     // Print out if User inputs are valid or invalid             
          if(username.contains("_") && username.length() <=5 && password != null && password.matches(".*[0-9].*") && password.length() >=8 && password.matches(".*[A-Z].*")) {
              System.out.println("Welcome " + firstName + " " + lastName + " it is great to see you again.");
          }else {
